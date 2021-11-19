4a46dcc912f70f2519476a1f6ab70e4d2bed486e
vbharathis <92903812+vbharathis@users.noreply.github.com>
2021-11-19 00:15:48 -0500

    Update cachematrix.R
    Submitting programming assignment after unit testing

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##Week3_Assignment
+##First function creation
+makecachematrix <-function( a= matrix())
+{
+  ##setting variable to null
+  inv  <- NULL
+  set <-function(b)
+    {
+      a <<-b
+      inv <<-NULL
+    }
+      get <-function() {a}
+      ##set value of inverse
+      setinverse <-function(inverse) {inv <<- inverse}
+      
+      ##Getting value of inverse
+      getinverse <-function()        {inv}
+      ##create list
+      list( set =set, get =get, setinverse= setinverse, getinverse=getinverse)
 }
+## End of make cache matrix
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##Second function cachesolve creation
+cachesolve <-function(a,...)
+{
+  inv <-a$getinverse ()
+  ## check if already cached
+  if (!is.null (inv))
+       {
+  ##Display message  
+          message("Retreiving cached data")
+          return (inv)
+       }
+  matr <- a$get()
+  inv <-solve(matr, ...)
+  a$setinverse(inv)
+  inv
 }
+## End of second function 
+
+##Testing
+showmatrix <-makecachematrix(matrix (1:4, nrow=2, ncol=2))
+showmatrix$get()
+cachesolve(showmatrix)
